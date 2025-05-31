c42cb66f3ba21f933e779adc1f3a3379bfc7b493
Carlos Sanchez <carlos.sanchez@heru-app.com>
2025-05-30 18:21:46 -0600

    Snapshop - CORS Headers disabled

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from tortoise import Tortoise, run_async
 from tortoise.contrib.fastapi import register_tortoise
+from fastapi.middleware.cors import CORSMiddleware
 from src.files.routes import router as file_router
 from src.voice.routes import router as audio_router
 from src.conversations.routes import router as conversations_router
@@ -11,8 +12,16 @@ app = FastAPI(
     title="Buddy API helps to implement real-life accessibility", version="0.1.0"
 )
 
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
-# 3. “Include” the router in the app
+# 3. "Include" the router in the app
 app.include_router(file_router)
 app.include_router(audio_router)
 app.include_router(conversations_router)
